7cab73f9c47b81737a59ca4e9164ba9ba54607a3
Samuel Cooper <2638137+StotheC@users.noreply.github.com>
2023-08-27 16:26:35 -0700

    excercises 4 thru 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+    if (x < 0 || y < 0) return "ERROR";
+    if (x > y) {
+        const temp = x;
+        x = y;
+        y = temp;
+    }
+    let num = 0;
+    for (let i = x; i <= y; i++) {
+        num += i;
+    }
+    return num;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(yr) {
+    if (yr % 4 === 0 && (yr % 100 != 0 || yr % 400 === 0)  ) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
