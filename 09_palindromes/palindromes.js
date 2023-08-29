dbfbe69c900b9e28d957e5dd546845c4c000f35f
Samuel Cooper <2638137+StotheC@users.noreply.github.com>
2023-08-29 09:43:12 -0700

    seven ate nine

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const backString = string.toLowerCase().replace(/[^a-z0-9]/g,"");
+    return backString.split("").reverse().join("") == backString;
 };
 
 // Do not edit below this line
