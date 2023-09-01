24bd9445a33e8a871dbb13a2af26da150b21c491
Samuel Cooper <2638137+StotheC@users.noreply.github.com>
2023-09-01 12:40:08 -0700

    finished excercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(e) {
+ if (e < 0) return "OOPS";
+ if (e === 0) return 0;
+
+ let prevFirst = 1;
+ let prevSecond = 0;
+ 
+ for (let i = 2; i <= e; i++) {
+    let current = prevFirst + prevSecond;
+    prevSecond = prevFirst;
+    prevFirst = current;
+ }
+
+ return prevFirst;
 
 };
 
